4e82cce016b7fb215ad4ef0de3eecd7626a333fc
Nuran Cetin <demir_nuran@hotmail.com>
2020-11-14 20:23:25 -0500

    Project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,13 @@ public class Hello {
         // more stuff
         // Mac end Window user more happy
 
+        //I have already publish on Github.com
+        // now I am ready to make more commits
+        // but do I need to share again?
+        // ABSOLUTELY NO
+        // It'S TIME COMMIT AND JUST PUSH
+
+
+
     }
 }
